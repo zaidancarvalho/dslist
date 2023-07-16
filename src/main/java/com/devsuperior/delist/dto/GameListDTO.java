499f1fdcb6332f3380438616fdb1bfb4d86723d8
zaidancarvalho <felipecfernandez@hotmail.com>
2023-07-16 13:55:04 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/delist/dto/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.delist.dto;
+
+import com.devsuperior.delist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		super();
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
