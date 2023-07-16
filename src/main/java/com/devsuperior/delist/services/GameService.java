e570c6369358e828908354d3b47c266ccfb44fcd
zaidancarvalho <felipecfernandez@hotmail.com>
2023-07-16 13:17:57 -0300

    GET game by id

--- src/main/java/com/devsuperior/delist/services/GameService.java
+++ src/main/java/com/devsuperior/delist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.delist.dto.GameDTO;
 import com.devsuperior.delist.dto.GameMinDTO;
 import com.devsuperior.delist.entities.Game;
 import com.devsuperior.delist.repositories.GameRepository;
@@ -14,10 +16,16 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
 
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		 return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
 	}
 }
