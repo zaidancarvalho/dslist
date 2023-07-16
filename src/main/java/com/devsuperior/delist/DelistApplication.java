8c3faa8847b97109226bcb7327c124136c8bb560
zaidancarvalho <felipecfernandez@hotmail.com>
2023-07-16 09:09:17 -0300

    Project created

---
+++ src/main/java/com/devsuperior/delist/DelistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.delist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelistApplication.class, args);
+	}
+
+}
